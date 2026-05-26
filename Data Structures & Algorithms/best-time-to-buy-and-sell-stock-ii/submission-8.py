9ac95b9e8026220f55d3fc054f14c7b3592f3c29
Om Arora <70465008+Om-Arora@users.noreply.github.com>
2026-05-26 11:49:56 -0500

    Add: best-time-to-buy-and-sell-stock-ii - submission-8

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-8.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # dp[i][0] = best profit made from day i to end 
+        #            when not holding stock
+        # dp[i][1] = best profit made from day i to end
+        #            when holding stock
+        # dp[i][0] = max(dp[i+1][0], -> nothing today
+        #                dp[i+1][1] - price[i] -> buy today
+        # )
+        # dp[i][1] = max(dp[i+1][1], -> nothing today
+        #                dp[i+1][0] + price[i]
+        # )
+        # O(n) solution, bottom up approach, space optimized
+        n = len(prices)
+        not_bought = 0
+        bought = prices[n - 1]
+        # prev = [0, prices[n-1]]
+
+        for day in range(n - 2, -1, -1):
+            not_bought, bought = max(not_bought, bought - prices[day]), max(bought, not_bought + prices[day])
+
+        return not_bought
